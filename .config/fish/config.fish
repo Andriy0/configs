cde46a10feb68a8460f5d14704721d08ce5c1d25
Andriy0 <andriy2152@gmail.com>
2020-11-27 20:51:52 +0200

    Minor edits

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
 # Variables
 set -x PATH "$PATH:~/.local/bin:/usr/local/go/bin"
+set -x QT_QPA_PLATFORMTHEME "qt5ct"
+set -x QT_STYLE_OVERRIDE "kvantum"
+set -x SAL_USE_VCLPLUGIN "gtk3"
 
 # If running from tty1, exec startx
 if [ (tty) = "/dev/tty1" ]
